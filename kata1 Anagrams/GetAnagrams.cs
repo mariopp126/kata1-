98a0ec9e91ea59438f48c09ceb1f32a9dc75f368
Mario <mpucheu126>
2021-05-20 17:49:52 -0400

    Pate faltante codigo anagrama

---
+++ kata1 Anagrams/GetAnagrams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata1_Anagrams
+{
+    public class GetAnagrams
+    {
+        public IEnumerable<string> Get(string path)
+        {
+            return File.ReadAllLines(path)
+            .GroupBy(e => string.Concat(e.OrderBy(b => b)))
+            .Where(e => e.Count() > 1)
+            .Select(e =>
+            {
+                return e.Key + " " + e.FirstOrDefault();
+            }
+           
+            );
+        }
+    }
+}
---
+++ kata1 Anagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata1_Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAnagrams getAnagrams = new GetAnagrams();
+            string path = @"C:\Users\Mario\Desktop\tendencias de aplic\kata1\kata1 Anagrams\words.txt";
+
+
+            List<string> anagramList = getAnagrams.Get(path).ToList();
+
+            foreach (var item in anagramList)
+            {
+                Console.WriteLine(item);
+                
+            }
+            Console.WriteLine(anagramList.Count());
+            Console.ReadKey();
+        }
+    }
+}
