a22eacfc63b86834e12e3d3c27d80006f29a2bfd
Mario <mpucheu126>
2021-05-20 17:23:45 -0400

    programa anagramas completo y test

---
+++ TestAnagrams/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAnagrams")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAnagrams")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44b8a0e6-76ac-4edb-a145-5d6a6bb2c367")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestAnagrams/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using kata1_Anagrams;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAnagrams
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GetAnagrams getAnagrams = new GetAnagrams();
+            string path = @"C:\Users\Mario\Desktop\tendencias de aplic\kata1\kata1 Anagrams\words.txt";
+            int estimateVal = 20683;
+
+            IEnumerable<string> anagrams = getAnagrams.Get(path);
+            Assert.AreEqual(anagrams.Count(), estimateVal);
+        }
+    }
+}
